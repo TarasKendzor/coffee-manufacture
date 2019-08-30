f711532f3fa8fc7718e954c2bf6ef246258fb320
TarasKendzor <taraskendzor@gmail.com>
2019-08-30 15:14:17 +0300

    fixed

--- main.js
+++ main.js
@@ -109,6 +109,14 @@ function changeAdress(e) {
 }
 
 
-
-
-
+window.onload = function() {
+  window.addEventListener('scroll', function() {
+    scroll = window.pageYOffset || document.documentElement.scroll;
+    scroll > 200 ? document.querySelector('.topButton').classList.add('fixed') : document.querySelector('.topButton').classList.remove('fixed');
+  });
+}
+$(document).ready(function () {
+  $('.topButton').click(function(){
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  });
+})
